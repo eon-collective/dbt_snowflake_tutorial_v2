4004be132cd1e6ed6d4568bc8659f48dff4d12d3
Janerose Njogu <Janerose.Njogu@eoncollective.com>
2023-05-04 09:32:59 +0000

    removed redundant files that caused dbt build to fail

--- models/snapshot_test.sql
+++
@@ -1,1 +0,0 @@
-select * from raw.dbt_janerose_snapshot.mock_orders
--- tests/assert_successful_stripe_payment.sql
+++
@@ -1,6 +0,0 @@
--- Ensure that stripe payment is successful. Status must be success.
--- Therefore return records where status is not success to make the test fail.
-select amount as stripe_amount
-from {{ ref("stg_payments") }}
-group by 1
-where status != "success"
