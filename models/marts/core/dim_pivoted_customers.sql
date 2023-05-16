1ef831d8587b77bb24b3ffe0f8a64bbe3cc97dc3
Janerose Njogu <Janerose.Njogu@eoncollective.com>
2023-05-16 08:44:54 +0000

    linting sql model file

--- models/marts/core/dim_pivoted_customers.sql
+++ models/marts/core/dim_pivoted_customers.sql
@@ -13,8 +13,8 @@ cust_first_names AS (
 -- 2nd CTE -> fetches all customer first names from model stg_customers
 pivoted_customers AS (
     SELECT 
-        ROW_NUMBER() OVER(ORDER BY first_name ASC) AS row_number,
         first_name,
+        ROW_NUMBER() OVER (ORDER BY first_name ASC) AS row_number,
         COUNT(customer_id) AS id_count
     FROM cust_first_names
     GROUP BY first_name
