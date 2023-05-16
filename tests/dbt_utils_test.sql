a001015d95661ae005f490c6a2019e42b98302c3
Janerose Njogu <Janerose.Njogu@eoncollective.com>
2023-05-16 09:04:46 +0000

    test file changes

--- tests/dbt_utils_test.sql
+++
@@ -1,7 +0,0 @@
--- -- formats current date and time
--- {{ dbt_utils.pretty_time(format='%Y-%m-%d %H:%M:%S') }}
-
--- -- LIST ALL COLUMNS FROM THE MODEL
--- SELECT
--- {{ dbt_utils.star(ref('stg_customers')) }}
--- FROM {{ ref('stg_customers') }}
